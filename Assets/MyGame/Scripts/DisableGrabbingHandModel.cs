fa1ca53da1727967b2ec594567d76e2d5e16a093
Sxingzz <saohuynh223@gmail.com>
2024-09-20 16:43:13 +0700

    [add] Hide Hand if grab space ice, complete trash votex material

---
+++ Assets/MyGame/Scripts/DisableGrabbingHandModel.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DisableGrabbingHandModel : MonoBehaviour
+{
+    public GameObject leftHandModel;
+    public GameObject rightHandModel;
+
+    private void Start()
+    {
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(HideGrabbingHand);
+        grabInteractable.selectExited.AddListener(ShowGrabbingHand);
+    }
+
+    public void HideGrabbingHand(SelectEnterEventArgs args)
+    {
+        Debug.Log("Object grabbed: " + args.interactableObject);
+        Debug.Log("Interactable Object Tag: " + args.interactableObject.transform.tag);
+        if (args.interactorObject.transform.tag == "Left Hand")
+        {
+            leftHandModel.SetActive(false);
+        }
+        else if (args.interactorObject.transform.tag == "Right Hand")
+        {
+            rightHandModel.SetActive(false);
+        }
+    }
+
+    public void ShowGrabbingHand(SelectExitEventArgs args)
+    {
+        if (args.interactorObject.transform.tag == "Left Hand")
+        {
+            leftHandModel.SetActive(true);
+        }
+        else if (args.interactorObject.transform.tag == "Right Hand")
+        {
+            rightHandModel.SetActive(true);
+        }
+    }
+
+}
